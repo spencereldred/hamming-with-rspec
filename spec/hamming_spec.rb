196a5d993703d679bdb10bac4df768435b9362df
Hunter Gillane <hunter.gillane@gmail.com>
2014-06-23 10:17:17 -0600

    remove pending specs

--- spec/hamming_spec.rb
+++ spec/hamming_spec.rb
@@ -7,32 +7,26 @@ describe "hamming" do
   end
 
   it "calculates the distance for single nucleotide strand" do
-    pending
     expect(hamming("A", "G")).to eq(1)
   end
 
   it "calculates the distance between small strands" do
-    pending
     expect(hamming("AG", "CT")).to eq(2)
   end
 
   it "calculates the distance between two other small strands" do
-    pending
     expect(hamming("AT", "CT")).to eq(1)
   end
 
   it "calculates the distance in longer strands" do
-    pending
     expect(hamming("GGACG", "GGTCG")).to eq(1)
   end
 
   it "ignores the extra length in the first strand when it is longer" do
-    pending
     expect(hamming("AAAG", "AAA")).to eq(0)
   end
 
   it "ignores the extra length in the second strand when it is longer" do
-    pending
     expect(hamming("AAA", "AAAG")).to eq(0)
   end
 end
