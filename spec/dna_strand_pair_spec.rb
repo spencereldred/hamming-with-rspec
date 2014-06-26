c5917c9ca9f836891e8abfe8eb1e4086cd31480f
Hunter Gillane <hunter.gillane@gmail.com>
2014-06-25 22:07:23 -0600

    actually call hamming distance method in specs

--- spec/dna_strand_pair_spec.rb
+++ spec/dna_strand_pair_spec.rb
@@ -7,27 +7,27 @@ describe DnaStrandPair do
   end
 
   it "calculates the distance between identical strands" do
-    expect(strand_pair("A", "A")).to eq(0)
+    expect(strand_pair("A", "A").hamming_distance).to eq(0)
   end
 
   it "calculates the distance for single nucleotide strand" do
     skip
-    expect(strand_pair("A", "G")).to eq(1)
+    expect(strand_pair("A", "G").hamming_distance).to eq(1)
   end
 
   it "calculates the distance between small strands" do
     skip
-    expect(strand_pair("AG", "CT")).to eq(2)
+    expect(strand_pair("AG", "CT").hamming_distance).to eq(2)
   end
 
   it "calculates the distance between two other small strands" do
     skip
-    expect(strand_pair("AT", "CT")).to eq(1)
+    expect(strand_pair("AT", "CT").hamming_distance).to eq(1)
   end
 
   it "calculates the distance in longer strands" do
     skip
-    expect(strand_pair("GGACG", "GGTCG")).to eq(1)
+    expect(strand_pair("GGACG", "GGTCG").hamming_distance).to eq(1)
   end
 
   it "ignores the extra length in the first strand when it is longer" do
