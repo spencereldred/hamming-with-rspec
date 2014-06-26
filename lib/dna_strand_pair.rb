88157f41c5a5f8025426007c2a08864fc0b5d2eb
Spencer Eldred <eldredspencer@gmail.com>
2014-06-26 09:21:38 -0600

    solved

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,18 @@
 class DnaStrandPair
 
+  def initialize(strand_1, strand_2)
+    @strand_1 = strand_1
+    @strand_2 = strand_2
+  end
+
+  def hamming_distance
+    distance = 0
+    index = 0
+    while index < @strand_1.length && index < @strand_2.length
+      distance += 1 if @strand_1[index] != @strand_2[index]
+      index += 1
+    end
+    distance
+  end
+
 end
--- spec/dna_strand_pair_spec.rb
+++ spec/dna_strand_pair_spec.rb
@@ -11,32 +11,26 @@ describe DnaStrandPair do
   end
 
   it "calculates the distance for single nucleotide strand" do
-    skip
     expect(strand_pair("A", "G").hamming_distance).to eq(1)
   end
 
   it "calculates the distance between small strands" do
-    skip
     expect(strand_pair("AG", "CT").hamming_distance).to eq(2)
   end
 
   it "calculates the distance between two other small strands" do
-    skip
     expect(strand_pair("AT", "CT").hamming_distance).to eq(1)
   end
 
   it "calculates the distance in longer strands" do
-    skip
     expect(strand_pair("GGACG", "GGTCG").hamming_distance).to eq(1)
   end
 
   it "ignores the extra length in the first strand when it is longer" do
-    skip
-    # write this test yourself!
+    expect(strand_pair("GGACGTCGT", "GGTCG").hamming_distance).to eq(1)
   end
 
   it "ignores the extra length in the second strand when it is longer" do
-    skip
-    # write this test yourself!
+    expect(strand_pair("GGACG", "GGTCGCGT").hamming_distance).to eq(1)
   end
 end
